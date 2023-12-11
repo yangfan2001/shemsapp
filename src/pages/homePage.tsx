63a857d3fb046d1948514b323b826c1ec2ff7d84
Weilin Ye <weilin@weilindndiannao.mynetworksettings.com>
2023-12-10 21:24:23 -0500

    added email on navbar

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,27 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import constants from "../constants"; // Assuming constants is defined in this file
-
-const getEmail = async () => {
-  return await axios.get(`${constants.ENDPOINT_URL}/location/test`);
-};
+import React from "react";
 
 export default function HomePage() {
-  const [res, setRes] = useState({ success: false, message: "NULL" });
-
-  useEffect(() => {
-    const fetchEmail = async () => {
-      try {
-        const response = await getEmail();
-        setRes(response.data);
-      } catch (error) {
-        // Handle error
-        console.error("Error fetching email:", error);
-      }
-    };
-
-    fetchEmail();
-  }, []); // Empty dependency array means this effect runs once on mount
-
-  return <div>{res.success ? res.message : "None"}</div>;
+  return <div>hi</div>;
 }
