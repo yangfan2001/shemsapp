dc844c5e3318084a444031db3ad7ea9a699e8bb5
Weilin Ye <weilin@weilindndiannao.mynetworksettings.com>
2023-12-11 11:18:25 -0500

    added route for locationpage

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,13 +1,13 @@
-import Navbar from './navbar/Navbar';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/homePage';
-import LoginPage from '../pages/login/loginPage';
-import RegisterPage from '../pages/register/registerPage';
-import AccountInfoPage from '../pages/account/AccountPage';
+import Navbar from "./navbar/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/homePage";
+import LoginPage from "../pages/login/loginPage";
+import RegisterPage from "../pages/register/registerPage";
+import AccountInfoPage from "../pages/account/AccountPage";
+import MyLocationPage from "../pages/location/MyLocationPage";
 const Layout: React.FC = () => {
   return (
-
     <>
       <Router>
         <Navbar />
@@ -16,11 +16,12 @@ const Layout: React.FC = () => {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/account" element={<AccountInfoPage />} />
+          <Route path="/location" element={<MyLocationPage />} />
           <Route path="*" element={<h1>Not Found</h1>} />
-        </Routes></Router>
+        </Routes>
+      </Router>
     </>
-
-  )
-}
+  );
+};
 
 export default Layout;
