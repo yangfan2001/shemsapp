5b3db5fa76f4618226bd081b40bbd9b66bb452ac
Weilin Ye <weilin@weilindndiannao.mynetworksettings.com>
2023-12-16 22:49:45 -0500

    added selection on display mode: day|month

--- src/pages/energy/Playground.tsx
+++ src/pages/energy/Playground.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid"; // Grid version 1
 import { getCustomerEnergyPerDay } from "../../services/energy";
 import { $TODAY } from "../../constants";
-import EnergyPieChart from "../../components/EnergyPieChart";
+import EnergyBarChart from "../../components/EnergyBarChart";
 import dayjs, { Dayjs } from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import {
@@ -13,6 +13,13 @@ import {
 import { PieChart } from "recharts";
 import DeviceTypePieChart from "../../components/chart/DeviceTypePieChart";
 import LocationEnergyPieChart from "../../components/chart/LocationEnergyPieChart";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 
 export default function Playground() {
   const [energyPerDay, setEnergyPerDay] = useState([]);
@@ -21,6 +28,15 @@ export default function Playground() {
   const initStart: Date = new Date($TODAY);
   initStart.setMonth(initStart.getMonth() - 1);
   const [displayStart, setDisplayStart] = useState<Date>(initStart);
+  const [displayMode, setDisplayMode] = React.useState<"day" | "month">("day");
+
+  const handleDisplayMode = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === "day") {
+      setDisplayMode("day");
+    } else {
+      setDisplayMode("month");
+    }
+  };
 
   const [displayRange, setValue] = React.useState<DateRange<Dayjs>>([
     dayjs(initStart),
@@ -59,7 +75,7 @@ export default function Playground() {
   return (
     <>
       <Grid container padding={2}>
-        <Grid xs={12}>
+        <Grid xs={8}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DateRangePicker
               value={displayRange}
@@ -68,11 +84,25 @@ export default function Playground() {
             />
           </LocalizationProvider>
         </Grid>
+        <Grid xs={4}>
+          <FormControl>
+            <FormLabel>Display By</FormLabel>
+            <RadioGroup value={displayMode} onChange={handleDisplayMode}>
+              <FormControlLabel value="day" control={<Radio />} label="Day" />
+              <FormControlLabel
+                value="month"
+                control={<Radio />}
+                label="Month"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
         <Grid xs={12}>
-          <EnergyPieChart
+          <EnergyBarChart
             data={energyPerDay}
             start={displayStart}
             end={displayEnd}
+            groupBy={displayMode}
           />
         </Grid>
 
