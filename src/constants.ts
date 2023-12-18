53622f52906c83df15a2276768aa909b6c06d706
Weilin Ye <weilin@Weilindebijibendiannao.local>
2023-12-17 22:26:35 -0500

    added some energy type

--- src/constants.ts
+++ src/constants.ts
@@ -27,4 +27,29 @@ export type ModifyLocationData = {
     numOccupants: number | null; 
 }
 
+export type EnergyInfo = {
+    date: string;
+    energy: number;
+  };
+
+export type EnergyInfoByLocationDate = {
+    location_id: number;
+    energy: [];
+}
+
+export type Location = {
+    customer_id: number;
+    location_id: number;
+    location_city: string;
+    location_state: string;
+    location_zipcode: string;
+    location_street_num: number;
+    location_street_name: string;
+    location_unit_number: string;
+    square_feet: number;
+    num_bedrooms: number;
+    num_occupants: number;
+    start_date: string;
+  };
+
 export const $TODAY = new Date('2022-12-31T23:59:59');
